a885e5ba4ef39f881b6e98fdf68198296f066860
alexanderjamesjackson <alex.jackson@new.ox.ac.uk>
2024-08-08 11:49:03 +0100

    Added diamondconfig script and added to sim_mpc, still dimension issues

---
+++ mpc/diamond_I_configuration_v5.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+def diamond_I_configuration_v5(RMorigx, RMorigy, square_config=False):
+    TOT_BPM = 173
+    TOT_CM = 172
+
+    # This is the configuration implemented on the 13.09.2022 at DLS
+    if not square_config:
+        bad_bpm_x = np.array([32, 75, 78, 120, 137, 155])
+        bad_bpm_y = np.array([18, 25, 62, 75, 78, 83, 137, 141])
+        bad_cm_x = np.array([76])
+        bad_cm_y = np.array([76])
+    else:
+        bad_bpm_x = np.array([32, 75, 78, 120, 137, 155])
+        bad_bpm_y = np.array([18, 25, 62, 75, 78, 83, 137, 141])
+        bad_cm_x = np.array([76, 32, 120, 137, 155])
+        bad_cm_y = np.array([76, 18, 25, 62, 83, 137, 141])
+
+    bad_bpm_x = np.sort(bad_bpm_x)
+    bad_bpm_y = np.sort(bad_bpm_y)
+    bad_cm_x = np.sort(bad_cm_x)
+    bad_cm_y = np.sort(bad_cm_y)
+
+    # Generate IDs as used by the FOFB
+    id_to_bpm_x = np.arange(1, TOT_BPM + 1)
+    id_to_bpm_x = np.delete(id_to_bpm_x, bad_bpm_x - 1)
+    id_to_bpm_y = np.arange(1, TOT_BPM + 1)
+    id_to_bpm_y = np.delete(id_to_bpm_y, bad_bpm_y - 1)
+    id_to_cm_x = np.arange(1, TOT_CM + 1)
+    id_to_cm_x = np.delete(id_to_cm_x, bad_cm_x - 1)
+    id_to_cm_y = np.arange(1, TOT_CM + 1)
+    id_to_cm_y = np.delete(id_to_cm_y, bad_cm_y - 1)
+
+    # Ensure full rank
+    assert np.linalg.matrix_rank(RMorigx[np.ix_(id_to_bpm_x - 1, id_to_cm_x - 1)]) == len(id_to_bpm_x)
+    assert np.linalg.matrix_rank(RMorigy[np.ix_(id_to_bpm_y - 1, id_to_cm_y - 1)]) == len(id_to_bpm_y)
+
+    for i in bad_bpm_x:
+        assert i not in id_to_bpm_x
+    for i in bad_cm_x:
+        assert i not in id_to_cm_x
+    for i in bad_bpm_y:
+        assert i not in id_to_bpm_y
+    for i in bad_cm_y:
+        assert i not in id_to_cm_y
+
+    return id_to_bpm_x, id_to_cm_x, id_to_bpm_y, id_to_cm_y
--- mpc/simulation_mpc.py
+++ mpc/simulation_mpc.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy.sparse as sparse
 from scipy.io import loadmat
 import sim_mpc_OSQP
+import diamond_I_configuration_v5 as DI
 
 # Load data from .mat files
 system_path = '../data/mpc_data_02092022_nd8.mat'
@@ -13,6 +14,13 @@ RM_data = loadmat(fname_RM)
 n_samples = 100
 n_delay = 9
 RMorigx = RM_data['Rmat'][0][0][0]
+square_config = True
+id_to_bpm_x, id_to_cm_x, id_to_bpm_y, id_to_cm_y = DI.diamond_I_configuration_v5(RMorigx, RMorigx, square_config)
+RMx = RMorigx[np.ix_(id_to_bpm_x, id_to_cm_x)]
+#Current issue, RM matrix is too small 167x167
+#Shrinking state transitions a solution? Currently 168x168
+
+
 TOT_BPM = np.size(RMorigx,0)
 dist = np.ones((TOT_BPM,1)) * np.ones((1,n_samples)) * 10
 Ap = mat_data['Ap_x']
@@ -28,8 +36,20 @@ Lxd_obs = mat_data['Kfd_x']
 P_mpc = mat_data['P_x']
 R_mpc = mat_data['Rlqr_x']
 J_mpc = np.transpose(Bo) @ P_mpc @ Ao + R_mpc
+ny = np.size(RMx, 0)
+nu = np.size(RMx, 1)
+nx = nu
+
+S_sp_pinv = np.linalg.pinv( np.block([
+    [np.eye(nx) - Ao, -Bo],
+    [Co, np.zeros((ny,nu))]
+]) )
 
 
+q_mat_x0 = np.transpose(Bo) @ P_mpc @ Ao
+q_mat_xd =(-(np.hstack( ((np.transpose(Bo) @ P_mpc) , R_mpc))) @ np.vstack((S_sp_pinv_x,S_sp_pinv_u))@ -Cd
+
+)
 
 
 
